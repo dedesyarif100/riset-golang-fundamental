ecb937bcafef801a9e505635c37a57cbed44b141
dedesyarif <dedesyarif177@gmail.com>
2022-05-21 11:02:53 +0700

    Add new materi golang

---
+++ pemrograman-web-go-dasar/B.15.AJAX-JSON-Response/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+import "net/http"
+import "encoding/json"
+
+func main() {
+    http.HandleFunc("/", ActionIndex)
+
+    fmt.Println("server started at localhost:9000")
+    http.ListenAndServe(":9000", nil)
+}
+
+func ActionIndex(w http.ResponseWriter, r *http.Request) {
+    data := [] struct {
+        Name string
+        Age  int
+    } {
+        { "Richard Grayson", 24 },
+        { "Jason Todd", 23 },
+        { "Tim Drake", 22 },
+        { "Damian Wayne", 21 },
+    }
+
+    // B.15. AJAX JSON Response
+    // jsonInBytes, err := json.Marshal(data)
+    // if err != nil {
+    //     http.Error(w, err.Error(), http.StatusInternalServerError)
+    //     return
+    // }
+    // w.Header().Set("Content-Type", "application/json")
+    // w.Write(jsonInBytes)
+
+    // B.15.3. JSON Response menggunakan JSON.Encoder
+    w.Header().Set("Content-Type", "application/json")
+    err := json.NewEncoder(w).Encode(data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+}
